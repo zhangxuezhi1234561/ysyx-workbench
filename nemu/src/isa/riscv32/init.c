0ef775a2e9ded71156dfe1d4fa7c5ad4099f2f75
tracer-ysyx <tracer@ysyx.org>
2023-09-17 21:09:33 +0800

    > compile NEMU ysyx_22040000 张三 Linux pro-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:33 up  8:24,  4 users,  load average: 0.18, 0.18, 0.18

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -18,7 +18,7 @@
 
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
-static const uint32_t img [] = {
+volatile static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
   0x00028823,  // sb  zero,16(t0)
   0x0102c503,  // lbu a0,16(t0)
