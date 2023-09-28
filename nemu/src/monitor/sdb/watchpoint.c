1676078205d34c2c24aa38e46ff9adc630729d13
tracer-ysyx <tracer@ysyx.org>
2023-09-28 18:08:58 +0800

    > compile NEMU ysyx_22040000 张三 Linux pro-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:08:58 up  5:18,  5 users,  load average: 0.23, 0.19, 0.20

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -98,8 +98,8 @@ void info_wp()
 	int i = 0;
 	while(head[i].next != NULL)
 	{
-//		panic("Num       Type          Disp     Enb      Address    What");
-		panic("%d        watchpoint    keep                         %s",head[i].NO,head[i].what);
+		Log("Num       Type          Disp     Enb      Address    What");
+		Log("%d        watchpoint    keep                         %s",head[i].NO,head[i].what);
 		i++;
 	}
 }
