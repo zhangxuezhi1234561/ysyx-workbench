0cee4fcac14e0db3424fe3c4997d6b195c560c5a
tracer-ysyx <tracer@ysyx.org>
2023-10-02 16:13:07 +0800

    > sim RTL ysyx_22040000 张学智 Linux pro-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:13:07 up  4:37,  5 users,  load average: 0.38, 0.43, 0.37

--- npc/Nvboard/csrc/ex7_top.cpp
+++ npc/Nvboard/csrc/ex7_top.cpp
@@ -10,10 +10,19 @@ static void single_cycle(){
 	dut.clk = 1; dut.eval();
 }
 
+static void reset(int n)
+{
+	dut.rst = 1;
+	while(n-->0) single_cycle();
+	dut.rst = 0;
+}
+
 int main()
 {
 	nvboard_bind_all_pins(&dut);
 	nvboard_init();
+
+	reset(10);
 
 	while(1)
 	{
